1e9ae9ab579bfa297c9e9302a09d83549f8d49ca
mysticlt <932721231@qq.com>
2019-09-23 15:37:21 +0800

    文章的类别查询

--- web_back/js/utils/config.js
+++ web_back/js/utils/config.js
@@ -10,5 +10,7 @@ var base_url = 'http://localhost:8000';
 var urlList = {
     user_login:base_url+'/admin/login',
     user_logout:base_url+'/admin/logout',
-    user_info:base_url+'/admin/getuser'
+    user_info:base_url+'/admin/getuser',
+
+    category_show:base_url+'/admin/category_search',
 };
